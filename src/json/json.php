cede1d563c46f5c12737af404ec9430a5f8ed9f7
Olde16 <oletaege@gmail.com>
2026-04-30 11:37:59 +0200

    First Addition

---
+++ src/json/json.php
@@ -0,0 +1,24 @@
+<?php namespace DB_API\JSON; class StaDa {
+    private $rawData;
+    public function __construct($JSON_Data) {
+        $this->rawData = $JSON_Data;
+    }
+}
+
+namespace DB_API\JSON; class FaSta {
+    public function __construct() {
+        
+    }
+}
+
+namespace DB_API\JSON; class RIS_Stations {
+    public function __construct() {
+        
+    }
+}
+
+namespace DB_API\JSON; class Timetables {
+    public function __construct() {
+        
+    }
+}
